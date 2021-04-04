e95eb70c6ebe96380c30f5279f73ad29c84eb422
Taichi Soma <sosomasox@gmail.com>
2021-04-04 17:35:36 +0900

    Update

--- apply_node-role4master.sh
+++ apply_node-role4master.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 kubectl label node `hostname` node-role.kubernetes.io/master=''
-kubectl label node `hostname` node-role.kubernetes.io/control-plane=''
+kubectl label node `hostname` node-role.kubernetes.io/controlplane=''
 kubectl label node `hostname` node-role.kubernetes.io/etcd=''
 kubectl taint node `hostname` node-role.kubernetes.io/master=:NoSchedule
-
+kubectl taint node `hostname` node-role.kubernetes.io/controlplane=:NoSchedule
+kubectl taint node `hostname` node-role.kubernetes.io/etcd=:NoExecute
 exit 0
