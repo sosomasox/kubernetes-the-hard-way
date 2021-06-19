53340487a1a75764a47b675360dd8b30585627c1
Taichi Soma <sosomasox@gmail.com>
2021-06-19 10:05:00 +0900

    Clean

--- manifest/master/metrics-server/setup.sh
+++ manifest/master/metrics-server/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-kubectl -n kube-system create configmap kubernetes-front-proxy-ca --from-file=kubernetes-front-proxy-ca.pem=/var/lib/kubernetes/kubernetes-front-proxy-ca.pem -o yaml | kubectl -nkube-system replace configmap kubernetes-front-proxy-ca -f -
+kubectl -n kube-system create configmap kubernetes-front-proxy-ca --from-file=kubernetes-front-proxy-ca.pem=/var/lib/kubernetes/kubernetes-front-proxy-ca.pem -o yaml | kubectl -n kube-system replace configmap kubernetes-front-proxy-ca -f -
 
 exit 0
