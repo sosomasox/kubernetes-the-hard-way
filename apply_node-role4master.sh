b46131a758eb04974ca5e24767f5c5f8e63cba89
Taichi Soma <sosomasox@gmail.com>
2021-06-19 09:27:40 +0900

    Update to execute as a shell

--- apply_node-role4master.sh
+++ apply_node-role4master.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-kubectl label node `hostname` node-role.kubernetes.io/master=''
-kubectl label node `hostname` node-role.kubernetes.io/controlplane=''
-kubectl label node `hostname` node-role.kubernetes.io/etcd=''
-kubectl taint node `hostname` node-role.kubernetes.io/master=:NoSchedule
-kubectl taint node `hostname` node-role.kubernetes.io/controlplane=:NoSchedule
-kubectl taint node `hostname` node-role.kubernetes.io/etcd=:NoSchedule
+MASTER1_HOSTNAME=k8s-master1
+MASTER2_HOSTNAME=k8s-master2
+MASTER3_HOSTNAME=k8s-master3
+
+for instance in ${MASTER1_HOSTNAME} ${MASTER2_HOSTNAME} ${MASTER3_HOSTNAME}; do
+	kubectl label node $instance node-role.kubernetes.io/master=''
+	kubectl label node $instance node-role.kubernetes.io/controlplane=''
+	kubectl label node $instance node-role.kubernetes.io/etcd=''
+	kubectl taint node $instance node-role.kubernetes.io/master=:NoSchedule
+	kubectl taint node $instance node-role.kubernetes.io/controlplane=:NoSchedule
+	kubectl taint node $instance node-role.kubernetes.io/etcd=:NoSchedule
+done
 
 exit 0
